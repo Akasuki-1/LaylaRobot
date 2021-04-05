61f64123a3ea773a99692d8738e780afd6446110
Hacker <78583783+DangerhackerRJ@users.noreply.github.com>
2021-04-05 14:55:22 +0530

    Update bans.py

--- LaylaRobot/modules/bans.py
+++ LaylaRobot/modules/bans.py
@@ -399,7 +399,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(KICKME_HANDLER)
 
-__mod_name__ = "Bans"
+__mod_name__ = "Bᴀɴs 🛑"
 __handlers__ = [
     BAN_HANDLER, TEMPBAN_HANDLER, KICK_HANDLER, UNBAN_HANDLER, ROAR_HANDLER,
     KICKME_HANDLER
